fe7e7a87c517c2c91f1c40b6acbbb0479303fb55
TanguturiArchana <150064169+TanguturiArchana@users.noreply.github.com>
2026-02-09 21:32:25 +0530

    Updated workerService.js

--- frontend/src/services/workerService.js
+++ frontend/src/services/workerService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8083/api";
+const API_BASE = "https://shramsaathibackend.onrender.com/api";
 
 export const workerService = {
   // Get all workers
