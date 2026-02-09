378e060a096e94d2214b3b73a850cb809668ca34
TanguturiArchana <150064169+TanguturiArchana@users.noreply.github.com>
2026-02-09 21:31:29 +0530

    Updated jobService.js

--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8083/api";
+const API_BASE_URL = "https://shramsaathibackend.onrender.com/api";
 
 export const jobService = {
   //  Get all jobs
